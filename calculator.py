449cbd9b93faa6c4084c78848ba3f44893174339
Hanna Trush <annetta2248@gmail.com>
2026-07-25 22:27:10 +0200

    Version 5: Add calculator loop and error handling

--- calculator.py
+++ calculator.py
@@ -1,22 +1,30 @@
 print("Hello!")
 print("Welcome to the Calculator program.")
 
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
-operation = input("What operation do you want to perform on the numbers? ")
+while True:
+    first_number = int(input("Enter the first number: "))
+    second_number = int(input("Enter the second number: "))
+    operation = input("What operation do you want to perform on the numbers? ")
 
-if operation == "+" :
-    total_addition = first_number + second_number
-    print(f"{first_number} + {second_number} = {total_addition}")
+    if operation == "+" :
+        addition = first_number + second_number
+        print(f"{first_number} + {second_number} = {addition}")
 
-if operation == "-" :
-    total_subtraction = first_number - second_number
-    print(f"{first_number} - {second_number} = {total_subtraction}")
+    elif operation == "-":
+        subtraction = first_number - second_number
+        print(f"{first_number} - {second_number} = {subtraction}")
 
-if operation == "*" :
-    total_multiplication = first_number * second_number
-    print(f"{first_number} * {second_number} = {total_multiplication}")
+    elif operation == "*":
+        multiplication = first_number * second_number
+        print(f"{first_number} * {second_number} = {multiplication}")
 
-if operation == "/" :
-    total_division = first_number / second_number
-    print(f"{first_number} / {second_number} = {total_division}")
+    elif operation == "/":
+        if second_number != 0:
+            division = first_number / second_number
+            print(f"{first_number} / {second_number} = {division}")
+        else:
+            print("You cannot divide by zero.")
+    else :
+        print("Invalid operation.")
+
+    
