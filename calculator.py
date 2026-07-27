74ee52fefa9ee43d73bb7ef0a0300155d84091d2
Hanna Trush <annetta2248@gmail.com>
2026-07-27 21:37:36 +0200

    Version 7: Add power, square root, and percentage operations

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 print("Hello!")
 print("Welcome to the Calculator program.")
 
@@ -7,14 +9,26 @@ while True:
     while True:
         operation = input("What operation do you want to perform on the numbers? ")
         
-        if operation == "+" or operation == "-" or operation == "*" or operation == "/" :
+        if operation == "%" or operation == "+" or operation == "-" or operation == "*" or operation == "**"  or operation == "sqrt" or operation == "/" :
             break
-        else :
-            print("You can use the following operations: +, -, *, /")
+        else:
+            print("You can use the following operations: sqrt, %, +, -, *, **, /")
 
-    second_number = int(input("Enter the second number: "))
+    if operation == "sqrt":
+        if first_number >= 0:
+            square_root = math.sqrt(first_number)
+            print(f"√{first_number} = {square_root}")
+        else:
+            print("You cannot calculate the square root of a negative number.")
 
-    if operation == "+" :
+    else:
+        second_number = int(input("Enter the second number: "))
+
+    if operation == "%":
+        percentage = (first_number / 100) * second_number
+        print(f"{first_number}% of {second_number} = {percentage}")
+
+    elif operation == "+" :
         addition = first_number + second_number
         print(f"{first_number} + {second_number} = {addition}")
 
@@ -26,10 +40,18 @@ while True:
         multiplication = first_number * second_number
         print(f"{first_number} * {second_number} = {multiplication}")
 
+    elif operation == "**":
+            power = first_number ** second_number
+            print(f"{first_number} ** {second_number} = {power}")
+
     elif operation == "/":
         if second_number != 0:
             division = first_number / second_number
             print(f"{first_number} / {second_number} = {division}")
         else:
             print("You cannot divide by zero.")
+
     
+
+
+# added error case for dividing by zero, error case for inserting non-operator characters
