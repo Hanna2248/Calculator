0a10d08032dc5a526c7e12ea85d29030b1b2d140
Hanna Trush <annetta2248@gmail.com>
2026-07-29 20:57:34 +0200

    Version 8: Add input validation with try/except and improve available operation check with in

--- calculator.py
+++ calculator.py
@@ -4,15 +4,21 @@ print("Hello!")
 print("Welcome to the Calculator program.")
 
 while True:
-    first_number = int(input("Enter the first number: "))
+
+    while True:
+        try:
+            first_number = int(input("Enter the first number: "))
+            break
+        except ValueError:
+            print("Please enter a number.")
 
     while True:
         operation = input("What operation do you want to perform on the numbers? ")
-        
-        if operation == "%" or operation == "+" or operation == "-" or operation == "*" or operation == "**"  or operation == "sqrt" or operation == "/" :
+
+        if operation in ["%", "+", "-", "*", "**", "sqrt", "/"]:
             break
         else:
-            print("You can use the following operations: sqrt, %, +, -, *, **, /")
+            print("You can use the following operations: %, +, -, *, **, sqrt, /")
 
     if operation == "sqrt":
         if first_number >= 0:
@@ -22,36 +28,36 @@ while True:
             print("You cannot calculate the square root of a negative number.")
 
     else:
-        second_number = int(input("Enter the second number: "))
+        while True:
+            try:
+                second_number = int(input("Enter the second number: "))
+                break
+            except ValueError:
+                print("Please enter a number.")
 
-    if operation == "%":
-        percentage = (first_number / 100) * second_number
-        print(f"{first_number}% of {second_number} = {percentage}")
+        if operation == "%":
+            percentage = (first_number / 100) * second_number
+            print(f"{first_number}% of {second_number} = {percentage}")
 
-    elif operation == "+" :
-        addition = first_number + second_number
-        print(f"{first_number} + {second_number} = {addition}")
+        elif operation == "+":
+            addition = first_number + second_number
+            print(f"{first_number} + {second_number} = {addition}")
 
-    elif operation == "-":
-        subtraction = first_number - second_number
-        print(f"{first_number} - {second_number} = {subtraction}")
+        elif operation == "-":
+            subtraction = first_number - second_number
+            print(f"{first_number} - {second_number} = {subtraction}")
 
-    elif operation == "*":
-        multiplication = first_number * second_number
-        print(f"{first_number} * {second_number} = {multiplication}")
+        elif operation == "*":
+            multiplication = first_number * second_number
+            print(f"{first_number} * {second_number} = {multiplication}")
 
-    elif operation == "**":
+        elif operation == "**":
             power = first_number ** second_number
             print(f"{first_number} ** {second_number} = {power}")
 
-    elif operation == "/":
-        if second_number != 0:
-            division = first_number / second_number
-            print(f"{first_number} / {second_number} = {division}")
-        else:
-            print("You cannot divide by zero.")
-
-    
-
-
-# added error case for dividing by zero, error case for inserting non-operator characters
+        elif operation == "/":
+            if second_number != 0:
+                division = first_number / second_number
+                print(f"{first_number} / {second_number} = {division}")
+            else:
+                print("You cannot divide by zero.")
